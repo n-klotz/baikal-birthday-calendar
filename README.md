ae23a61c3144d614651d9bd71d697be39fffc070
Nicolas Klotz <mail@nicolasklotz.de>
2016-10-26 15:07:40 +0200

    Rewritten for sqlite backend

--- README.md
+++ README.md
@@ -2,10 +2,24 @@ baikal-birthday-calendar
 ========================
 
 Python script to automatically create birthday calendar entries for a baikal server (http://baikal-server.com).
-Tested with baikal 0.2.7 and python 2.7.5 on CentOS 7. Works only with MySQL backend.
+
+Tested with baikal 0.4.6 and python 2.7.9 on Debian 8.6. Works only with SQLite backend.
+
+This script is based on a previous version by mcwolle for baikal and MySQL.
 
 Installation
 ========================
-- Copy script to /opt/birthdayCron
-- Modify the script and change the following parameters: sql user (sqluser), password (sqlpwd), database name (sqldb), file path to store a datafile (datafile), name of the birthday calendar (calendaruri) and displayed name of the birthday calendar (calendarname).
-- Add the script as cron (e.g. 00 * * * * mcwolle /opt/birthdayCron/birthdayCron.py)
+- Copy script e.g. to /var/www/baikal/birthday.cron/
+- Modify the script and change the following parameters:  
+    * filepath to sqlite database (sqlpath)
+    * name of the birthday calendar (calendaruri)
+    * displayed name of the birthday calendar (calendarname)
+    * time zone (calendartz)
+    * calendar description (calendardesc)
+    * displayed calendar color (calendarcolor)
+- Add the script as cron task
+
+Credits 
+========================
+- mcwolle : https://github.com/mcwolle/baikal-birthday-calendar
+- funkfux : https://github.com/fruux/Baikal/issues/38
--- birthdayCron.py
+++ birthdayCron.py
@@ -1,43 +1,46 @@
 #!/usr/bin/env python
-import MySQLdb
-import MySQLdb.cursors
+# -*- coding: utf-8 -*-
+
+import sqlite3
 import vobject
 from datetime import datetime, timedelta
 import hashlib
 import calendar
 import os.path
-import pickle
 
-sqlhost = 'localhost'
-sqluser = 'baikal'
-sqlpwd  = 'MySQL Password'
-sqldb   = 'baikal'
+##########
+# CONFIG #
+##########
 
+sqlpath = '/var/www/baikal/Specific/db/db.sqlite'
 calendaruri  = 'birthdays'
-calendarname = 'Geburtstage'
+calendarname = 'Birthdays'
+calendartz = 'Europe/Berlin'
+calendardesc = 'Automatically populated birthday calendar'
+calendarcolor = '#A6DCE9'
+bdaytext = 'Birthday of '
 
+### END CONFIG ###
+
+# dates
 now = datetime.utcnow()
 nowts = calendar.timegm(now.timetuple())
 
-# Load stored pickle file if existing
-datafile = '/opt/birthdayCron/birthdayCron.p'
-if os.path.isfile(datafile):
-    stored_addressbook_ctags = pickle.load(open(datafile, "rb"))
-else:
-    stored_addressbook_ctags = {}
+# db connections
+db = sqlite3.connect(sqlpath)
+db.text_factory = str
 
-db = MySQLdb.connect(host=sqlhost, user=sqluser, passwd=sqlpwd, db=sqldb)
 read_cur  = db.cursor()
 write_cur = db.cursor()
 
-# Load ctags of addressbooks and consolidate into one entry per principal
-current_addressbook_ctags = {}
-read_cur.execute('SELECT principaluri, ctag FROM addressbooks ORDER BY id ASC')
+# Load synctokens of addressbooks and consolidate into one entry per principal
+current_addressbook_synctokens = {}
+read_cur.execute('SELECT principaluri, synctoken FROM addressbooks ORDER BY id ASC')
 for addressbook in read_cur.fetchall():
-    if addressbook[0] not in current_addressbook_ctags:
-        current_addressbook_ctags[addressbook[0]] = str(addressbook[1])
+    if addressbook[0] not in current_addressbook_synctokens:
+        current_addressbook_synctokens[addressbook[0]] = str(addressbook[1])
     else:
-        current_addressbook_ctags[addressbook[0]] = current_addressbook_ctags[addressbook[0]]+str(addressbook[1])
+        current_addressbook_synctokens[addressbook[0]] = current_addressbook_synctokens[addressbook[0]]+str(addressbook[1])
 
 # Get principals from DB
 read_cur.execute('SELECT uri FROM principals')
@@ -45,67 +48,63 @@ for principal in read_cur.fetchall():
     # Set uri of principal
     uri = principal[0]
 
-    if uri not in stored_addressbook_ctags or (uri in stored_addressbook_ctags and stored_addressbook_ctags[uri] != current_addressbook_ctags[uri]):
-        # Get birthdays calendar from principal
-        read_cur.execute('SELECT id FROM calendars WHERE principaluri = %s and uri = %s', [uri, calendaruri])
-        row = read_cur.fetchone()
-    
-        if row == None:
-            # Create birthdays calendar if not existing
-            var = [uri, calendarname, calendaruri, 1, calendarname, 0, "", "", "VEVENT"]
-            write_cur.execute('INSERT INTO calendars (principaluri, displayname, uri, ctag, description, calendarorder, calendarcolor, timezone, components) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)', var)
-            calendarid = write_cur.lastrowid
-        else:
-            calendarid = row[0]
+    # Get birthdays calendar from principal
+    read_cur.execute('SELECT id FROM calendars WHERE principaluri = ? and uri = ?', [uri, calendaruri])
+    row = read_cur.fetchone()
 
-        # Clear calendar first
-        write_cur.execute('DELETE FROM calendarobjects WHERE calendarid = %s', [calendarid])
-            
-        read_cur.execute('SELECT c.carddata, c.uri FROM cards as c JOIN addressbooks as a ON c.addressbookid = a.id WHERE a.principaluri = %s', [uri])
-        for row in read_cur.fetchall():
-            card = vobject.readOne(row[0])
-            carduri = row[1]
+    if row == None:
+        # Create birthdays calendar if not existing
+        var = [uri, calendarname, calendaruri, 1, calendardesc, 0, calendarcolor, calendartz, "VEVENT"]
+        write_cur.execute('INSERT INTO calendars (principaluri, displayname, uri, synctoken, description, calendarorder, calendarcolor, timezone, components) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', var)
+        calendarid = write_cur.lastrowid
+    else:
+        calendarid = row[0]
 
-            if hasattr(card, 'bday'):
-                bday = datetime.strptime(card.bday.value, '%Y-%m-%d').date()
-                ca = vobject.newFromBehavior('vcalendar')
+    # Clear calendar first
+    write_cur.execute('DELETE FROM calendarobjects WHERE calendarid = ?', [calendarid])
 
-                # Create event
-                ev = ca.add('vevent')
-                ev.add('summary').value = card.fn.value
-                ev.add('dtstart').value = bday
-                ev.add('dtend').value = bday+timedelta(days=1)
-                ev.add('class').value = "public"
-                ev.add('created').value = now
-                ev.add('dtstamp').value = now
-                ev.add('last-modified').value = now 
-                ev.add('rrule').value = "FREQ=YEARLY;BYMONTHDAY="+str(bday.day)+";BYMONTH="+str(bday.month);
-                ev.add('transp').value = "TRANSPARENT"
-                ev.add('categories').value = ["Birthday"]
-                ev.add('x-microsoft-cdo-alldayevent').value = "TRUE"
+    read_cur.execute('SELECT c.carddata, c.uri FROM cards as c JOIN addressbooks as a ON c.addressbookid = a.id WHERE a.principaluri = ?', [uri])
+    for row in read_cur.fetchall():
+        card = vobject.readOne(row[0])
+        carduri = row[1]
 
-                # Create alarm
-                al = ev.add('valarm')
-                al.add('action').value = "Display"
-                al.add('trigger;related=end').value = "-PT16H"
+        if hasattr(card, 'bday'):
+            bday = datetime.strptime(card.bday.value, '%Y-%m-%d').date()
+            ca = vobject.newFromBehavior('vcalendar')
 
-                data = ca.serialize();
-                etag = hashlib.md5(data).hexdigest()
-                size = len(data)
-                newuri = str(carduri[:-4])+'.ics'
+            # Create event
+            ev = ca.add('vevent')
+            ev.add('summary').value = bdaytext+card.fn.value
+            ev.add('dtstart').value = bday
+            ev.add('dtend').value = bday+timedelta(days=1)
+            ev.add('class').value = "public"
+            ev.add('created').value = now
+            ev.add('dtstamp').value = now
+            ev.add('last-modified').value = now
+            ev.add('rrule').value = "FREQ=YEARLY;BYMONTHDAY="+str(bday.day)+";BYMONTH="+str(bday.month);
+            ev.add('transp').value = "TRANSPARENT"
+            ev.add('categories').value = ["Birthday"]
+            ev.add('x-microsoft-cdo-alldayevent').value = "TRUE"
 
-                # Insert data into DB
-                var = [data, newuri, calendarid, nowts, etag, size, 'VEVENT', nowts, nowts+300000000]
-                write_cur.execute('INSERT INTO calendarobjects (calendardata, uri,  calendarid, lastmodified, etag, size, componenttype, firstoccurence,lastoccurence) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)', var)
+            # Create alarm
+            al = ev.add('valarm')
+            al.add('action').value = "Display"
+            al.add('trigger;related=end').value = "-PT16H"
 
-                # Debug Output
-                # print "Inserted new entry with title "+card.fn.value+"."
+            data = ca.serialize();
+            etag = hashlib.md5(data).hexdigest()
+            size = len(data)
+            newuri = str(carduri[:-4])+'.ics'
 
-        # Update ctag in birthday calendar (to indicate changes)
-        write_cur.execute('UPDATE calendars SET ctag = ctag + 1 WHERE id = %s', [calendarid])
+            # Insert data into DB
+            var = [data, newuri, calendarid, nowts, etag, size, 'VEVENT', nowts, nowts+300000000]
+            write_cur.execute('INSERT INTO calendarobjects (calendardata, uri,  calendarid, lastmodified, etag, size, componenttype, firstoccurence,lastoccurence) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', var)
 
-        # Store current ctags of addressbooks to disk
-        pickle.dump(current_addressbook_ctags, open(datafile, "wb"))
+            # Debug Output
+            # print "Inserted new entry with title "+card.fn.value+"."
+
+    # Update synctoken in birthday calendar (to indicate changes)
+    write_cur.execute('UPDATE calendars SET synctoken = synctoken + 1 WHERE id = ?', [calendarid])
 
 db.commit()
 read_cur.close()
